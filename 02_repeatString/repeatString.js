6f880b8f9c426fabbad93bb659df14da1bdd6e1a
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-08 20:20:53 -0400

    add loop in function to repeat string specified number of times and add variable to host final returned string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+let finalString;
 
+const repeatString = function(string, num) {
+    let finalString = string;
+    for (let i = 1; i <= num; i++) {
+        return finalString += string; 
+    }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
