94d2daee1a938a8c1adec5b25d71b6fdc1b4eda6
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-10 19:29:29 -0400

    Enable subsequent tests and revise js to pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,24 @@
 const repeatString = function(string, num) {
     let finalString = '';
-    for (let i = 1; i <= num; i++) {
-        finalString += string; 
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 1; i <= num; i++) {
+         finalString += string; 
+        }
     }
     return finalString;
 };
 
 repeatString('hey', 3);
+
+repeatString('hello', 10);
+
+repeatString('hi', 1);
+
+repeatString('bye', 0);
+
+repeatString('goodbye', -1);
 
 // Do not edit below this line
 module.exports = repeatString;
