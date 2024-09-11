564c398410f13c63f87de0144c446b5cf901c3e5
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-10 20:27:29 -0400

    Develop function to split string parameter into array, reverse and join array into the final reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLett = string.split('');
+    let reverseLett = stringLett.reverse();
+    return reverseLett.join('');
 };
 
 // Do not edit below this line
