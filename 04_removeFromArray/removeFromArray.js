bd8f21a0fd84ce520b998dc313cf6ad157e739f1
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-11 21:28:57 -0400

    Add variable that isolates the argument and splices it out of the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, indexRemove) {
+    let indexNum = array.indexOf(indexRemove);
+    array.splice(indexNum, 1);
+    return array;
 };
 
 // Do not edit below this line
