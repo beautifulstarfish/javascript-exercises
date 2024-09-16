f44f560e8ef1ea73e49cc3337143a206452baadc
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-15 21:55:21 -0400

    Edit function to check integer and add together array of integers between arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(intA, intB) {
+    const rangeArray = [];
+    let finalSum;
+    if (Number.isInteger(intA && intB)) {
+        if (intA > intB) {
+            for (i = intA; i <= intA; i--) {
+                rangeArray.push(i);
+            }
+        } else {
+            for (i = intA; i >= intA; i++) {
+                rangeArray.push(i);
+            }
+        }
+        for (i = 0; i < rangeArray.length; i++) {
+            finalSum += rangeArray[i];
+        }
+    } else {
+        return 'ERROR';
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
