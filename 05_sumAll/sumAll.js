8719a1582bf4568fdad908ddacd9b3e46885b091
beautifulstarfish <164958630+beautifulstarfish@users.noreply.github.com>
2024-09-17 09:20:35 -0400

    Edit function to filter only integers and positive numbers which become an array to be added together in a loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,21 @@
 const sumAll = function(intA, intB) {
     const rangeArray = [];
-    let finalSum;
-    if (Number.isInteger(intA && intB)) {
+    let finalSum = 0;
+    if (Number.isInteger(intA) && Number.isInteger(intB) && intA >= 0 && intB >= 0) {
         if (intA > intB) {
-            for (i = intA; i <= intA; i--) {
+            for (i = intA; i >= intB; i--) {
                 rangeArray.push(i);
             }
         } else {
-            for (i = intA; i >= intA; i++) {
+            for (i = intA; i <= intB; i++) {
                 rangeArray.push(i);
             }
         }
-        for (i = 0; i < rangeArray.length; i++) {
-            finalSum += rangeArray[i];
-        }
     } else {
         return 'ERROR';
+    }
+    for (i = 0; i < rangeArray.length; i++) {
+        finalSum += rangeArray[i];
     }
     return finalSum;
 };
